d9cf06f945097a5769c29f14efa46de308cf11d3
Andres Ideus <snoweagle@snoweagle.net>
2023-09-24 18:17:13 -0700

    implement UCS filenaming and increment w defaults

--- process_audio_files.py
+++ process_audio_files.py
@@ -2,7 +2,6 @@ import os
 import shutil
 import yaml
 import time
-import numpy
 import soundfile as sf
 import sounddevice as sd
 from google.oauth2.service_account import Credentials
@@ -17,98 +16,90 @@ def load_config():
             config = yaml.safe_load(stream)
             
             # Expand any tildes (~) in paths
-            if 'sd_card_path' in config:
-                config['sd_card_path'] = os.path.expanduser(config['sd_card_path'])
-            
-            if 'google_drive_folder_id' in config:
-                config['google_drive_folder_id'] = os.path.expanduser(config['google_drive_folder_id'])
-
-            if 'service_json_path' in config:
-                config['service_json_path'] = os.path.expanduser(config['service_json_path'])
-                
+            for key in ['sd_card_path', 'google_drive_folder_id', 'service_json_path']:
+                if key in config:
+                    config[key] = os.path.expanduser(config[key])
             return config
         except yaml.YAMLError as exc:
             print(exc)
+            return None
 
-def play_first_5_seconds(audio_path):
-    # Read the audio file into a numpy array
+def play_slate(audio_path, slate_length):
     audio_data, samplerate = sf.read(audio_path, dtype='float32')
-
-    # Calculate the number of frames for 5 seconds
-    num_frames = int(5 * samplerate)
-
-    # Truncate or pad the audio data to have exactly 'num_frames' frames
+    num_frames = int(slate_length * samplerate)
     audio_data = audio_data[:num_frames]
-    
-    # Play the audio data
     print(f"Playing {audio_path}, sample rate: {samplerate}.")
     sd.play(audio_data, samplerate=samplerate)
     sd.wait()
 
 def push_to_google_drive(file_path, new_filename, folder_id, config):
-        
     try:
-        # Initialize the Drive v3 API
         SCOPES = ['https://www.googleapis.com/auth/drive.file']
-        creds = None
-
-        creds = Credentials.from_service_account_file(
-            config['service_json_path'],
-            scopes=SCOPES
-        )
-
+        creds = Credentials.from_service_account_file(config['service_json_path'], scopes=SCOPES)
         service = build('drive', 'v3', credentials=creds)
-
-        # Upload file to Google Drive
-        media = MediaFileUpload(file_path, mimetype='audio/wav')
         
-        request = service.files().create(
-            media_body=media,
-            body={
-                'name': new_filename,  # Using the new filename
-                'parents': [folder_id]  # ID of the folder you want to upload to
-            }
-        )
+        media = MediaFileUpload(file_path, mimetype='audio/wav')
+        request = service.files().create(media_body=media, body={
+            'name': new_filename,
+            'parents': [folder_id]
+        })
+        
         print(f"Uploading {file_path} as {new_filename}.")
         file = request.execute()
         print(f"Uploaded {file['name']} to Google Drive.")
 
     except HttpError as error:
         print(f"An HTTP error occurred: {error}")
-    except FileNotFoundError as e:
-        print(f"FileNotFoundError: {e}")
-        print(f"Args: {e.args}")
-    except Exception as e:
-        print(f"An unknown error occurred: {e}")
-
 def main():
     config = load_config()
     if not config:
         print("Error loading config file.")
         return
-
-    ignore_folders = ['F6_SETTINGS', 'FALSE_TAKE', 'TRASH']  # Add folder names to ignore here
+    
+    last_fxname = None
+    fxname_increment = 1
+    
+    ignore_folders = ['F6_SETTINGS', 'FALSE_TAKE', 'TRASH']
     folders_to_delete = []
+
     for root, dirs, files in os.walk(config['sd_card_path']):
-        # Skip ignored folders
         if any(ignore in root for ignore in ignore_folders):
             continue
-
+        
         wav_files = [f for f in files if f.endswith(".WAV")]
+        
         if wav_files:
             first_wav_file = wav_files[0]
             full_path = os.path.join(root, first_wav_file)
-            play_first_5_seconds(full_path)
+            play_slate(full_path, config['slate_length'])
 
-            label = input("Enter label: ")
-
+            if last_fxname:
+                print(f"Default fxname: {last_fxname} {fxname_increment + 1}")  # Incremented by 1
+            
+            fxname = input("Enter fxname: ").strip()
+            
+            if not fxname:
+                fxname = last_fxname
+                fxname_increment += 1
+            else:
+                last_fxname = fxname
+                fxname_increment = 1
+            
             for wav_file in wav_files:
-                new_filename = f"{label}_placeholder_{wav_file}"  # TODO: Update your filename structure as needed
+                parts = wav_file.split('_')
+                date = parts[0]
+                increment = parts[1]
+                track_info = '_'.join(parts[2:]).replace('.WAV', '')
+
+                mic_key = track_info.replace(".", "").replace(" ", "")
+                mic = config['mic'].get(mic_key, "UNKNOWN")
+
+                new_filename = f"CatID_{fxname} {fxname_increment}_{config['creator_id']}_{config['source_id']}_{date}-{mic}.WAV"
                 full_path = os.path.join(root, wav_file)
                 push_to_google_drive(full_path, new_filename, config['google_drive_folder_id'], config)
                 folders_to_delete.append(root)
-
-    # Ask to delete folders
+    
+    # Moved this section out of the inner for loop
     delete_folders = input("Do you want to delete processed folders? (yes/no): ").strip().lower()
     if delete_folders == 'yes':
         folders_to_delete = list(set(folders_to_delete))
